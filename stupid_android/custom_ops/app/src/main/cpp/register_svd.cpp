7d947f27b75e33267565c2ec2be98a92505b912c
Thomas Mueller <muelletm@gmail.com>
2020-10-17 23:44:47 +0200

    Adds basic SVD op.

--- stupid_android/custom_ops/app/src/main/cpp/register_svd.cpp
+++ stupid_android/custom_ops/app/src/main/cpp/register_svd.cpp
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 #include <android/log.h>
 #include <cinttypes>
 #include <cstring>
@@ -33,109 +17,176 @@
 #define LOGE(...) \
   ((void)__android_log_print(ANDROID_LOG_ERROR, "oh shit::", __VA_ARGS__))
 
+static tflite::FlatBufferModel* model_ = nullptr;
+static tflite::Interpreter* interpreter_ = nullptr;
+
+
 namespace tflite {
 namespace ops {
 namespace custom {
 
-TfLiteStatus SinPrepare(TfLiteContext* context, TfLiteNode* node) {
+TfLiteStatus SvdPrepare(TfLiteContext* context, TfLiteNode* node) {
+    LOGI("Prepare SVD");
     TF_LITE_ENSURE_EQ(context, NumInputs(node), 1);
-    TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
-    const TfLiteTensor* input = GetInput(context, node, 0);
-    TfLiteTensor* output = GetOutput(context, node, 0);
+    TF_LITE_ENSURE_EQ(context, NumOutputs(node), 3);
+    const TfLiteTensor* matrix = GetInput(context, node, 0);
+    TfLiteTensor* s = GetOutput(context, node, 0);
+    TfLiteTensor* u = GetOutput(context, node, 1);
+    TfLiteTensor* v = GetOutput(context, node, 2);
 
-    int num_dims = NumDimensions(input);
+    int num_dims = NumDimensions(matrix);
 
-    TfLiteIntArray* output_size = TfLiteIntArrayCreate(num_dims);
-    for (int i=0; i<num_dims; ++i) {
-        output_size->data[i] = input->dims->data[i];
+    TfLiteIntArray* s_output_size = TfLiteIntArrayCreate(num_dims - 1);
+    for (int i=0; i<num_dims - 1; ++i) {
+        s_output_size->data[i] = matrix->dims->data[i];
     }
 
-    return context->ResizeTensor(context, output, output_size);
+    TfLiteIntArray* u_output_size = TfLiteIntArrayCreate(num_dims);
+    for (int i=0; i<num_dims; ++i) {
+        u_output_size->data[i] = matrix->dims->data[i];
+    }
+
+    TfLiteIntArray* v_output_size = TfLiteIntArrayCreate(num_dims);
+    for (int i=0; i<num_dims; ++i) {
+        v_output_size->data[i] = matrix->dims->data[i];
+    }
+
+    TfLiteStatus status = context->ResizeTensor(context, s, s_output_size);
+    if (status != kTfLiteOk) {
+        return status;
+    }
+    status = context->ResizeTensor(context, u, u_output_size);
+    if (status != kTfLiteOk) {
+        return status;
+    }
+    status = context->ResizeTensor(context, v, v_output_size);
+    return status;
 }
 
-TfLiteStatus SinEval(TfLiteContext* context, TfLiteNode* node) {
-    using namespace tflite;
-    const TfLiteTensor* input = GetInput(context, node,0);
-    TfLiteTensor* output = GetOutput(context, node,0);
-
-    float* input_data = input->data.f;
-    float* output_data = output->data.f;
-
+void SetToZero(float* data, TfLiteTensor* tensor) {
     size_t count = 1;
-    int num_dims = NumDimensions(input);
+    int num_dims = NumDimensions(tensor);
     for (int i = 0; i < num_dims; ++i) {
-        count *= input->dims->data[i];
+        count *= tensor->dims->data[i];
     }
-
     for (size_t i=0; i<count; ++i) {
-        output_data[i] = sin(input_data[i]);
+        data[i] = 0.0;
     }
+}
+
+void Copy(float* src, float* trg, TfLiteTensor* tensor) {
+    size_t count = 1;
+    int num_dims = NumDimensions(tensor);
+    for (int i = 0; i < num_dims; ++i) {
+        count *= tensor->dims->data[i];
+    }
+    for (size_t i=0; i<count; ++i) {
+        trg[i] = src[i];
+    }
+}
+
+TfLiteStatus SvdEval(TfLiteContext* context, TfLiteNode* node) {
+    LOGI("Eval SVD");
+    using namespace tflite;
+    const TfLiteTensor* matrix = GetInput(context, node,0);
+    TfLiteTensor* s = GetOutput(context, node,0);
+    TfLiteTensor* u = GetOutput(context, node,1);
+    TfLiteTensor* v = GetOutput(context, node,2);
+
+    float* input_data = matrix->data.f;
+    float* s_data = s->data.f;
+    float* u_data = u->data.f;
+    float* v_data = v->data.f;
+
+    SetToZero(s_data, s);
+    Copy(input_data, u_data, u);
+    Copy(input_data, v_data, v);
     return kTfLiteOk;
 }
 
-TfLiteRegistration* Register_SIN() {
-    static TfLiteRegistration r = {nullptr, nullptr, SinPrepare, SinEval};
+TfLiteRegistration* Register_SVD() {
+    LOGI("Register SVD");
+    static TfLiteRegistration r = {nullptr, nullptr, SvdPrepare, SvdEval};
     return &r;
 }
 
-void Init() {
-    LOGI("Registering Sin.");
+std::string PrepareInterpreter(const std::string& modelPath) {
+    model_ = tflite::FlatBufferModel::BuildFromFile(modelPath.c_str()).release();
+    if (model_ == nullptr) {
+        return "No such file: '" + std::string(modelPath) + "'";
+    }
+
+    std::unique_ptr<Interpreter> interpreter;
     tflite::ops::builtin::BuiltinOpResolver resolver;
-    resolver.AddCustom("Sin", Register_SIN());
+    resolver.AddCustom("Svd", Register_SVD());
+    if (InterpreterBuilder(*model_, resolver)(&interpreter) != kTfLiteOk) {
+        return "Cannot build interpreter";
+    }
+    interpreter_ = interpreter.release();
+    return "";
+}
+
+void appendOutput(const int index, const std::string& name, std::string& message) {
+    TfLiteTensor* tensor = interpreter_->output_tensor(index);
+    message += name;
+    message += " :";
+
+    int length = 1;
+    message += "shape = (";
+    for (int dim=0; dim < tensor->dims->size; ++dim) {
+        message += " " + std::to_string(tensor->dims->data[dim]);
+        length *= tensor->dims->data[dim];
+    }
+    message += ")";
+
+    float* data = interpreter_->typed_output_tensor<float>(index);
+    message += "[";
+    for (int i=0; i < length; ++i) {
+        message += " " + std::to_string(data[i]);
+    }
+    message += "]";
+}
+
+std::string runSvD() {
+    if (interpreter_ == nullptr) {
+        return "ERROR: Interpreter is null.";
+    }
+    interpreter_->ResizeInputTensor(0, {1, 3, 3});
+
+    if (interpreter_->AllocateTensors() != kTfLiteOk) {
+        return "ERROR: Cannot allocate tensors";
+    }
+
+    float inputs[] = {0., 0., 1., 0., 1., 0., 1., 0., 0. };
+
+    float* input_data = interpreter_->typed_input_tensor<float>(0);
+    for (int i=0; i<9; i++) {
+        input_data[i] = inputs[i];
+    }
+
+    if (interpreter_->Invoke() != kTfLiteOk) {
+        return "ERROR: Cannot invoke";
+    }
+
+    // The graph should only use the s and u tensors.
+    if (interpreter_->outputs().size() != 2) {
+        LOGE("ERROR: Unexpected number of outputs: %d", interpreter_->outputs().size());
+        return "ERROR: Unexpected number of outputs: " + std::to_string(interpreter_->outputs().size());
+    }
+
+    std::string message;
+    appendOutput(0, "s", message);
+    message += "\n";
+    appendOutput(1, "u", message);
+    message += "\n";
+    return message;
 }
 
 }  // namespace custom
 }  // namespace ops
-
-std::string RunInterpreter(std::string modelFile) {
-    // Create model from file. Note that the model instance must outlive the
-    // interpreter instance.
-    auto model = tflite::FlatBufferModel::BuildFromFile(modelFile.c_str());
-    if (model == nullptr) {
-        LOGE("No such file: %s", modelFile.c_str());
-        return "ERROR: No such file: '" + std::string(modelFile) + "'";
-    }
-    // Create an Interpreter with an InterpreterBuilder.
-    std::unique_ptr<Interpreter> interpreter;
-    tflite::ops::builtin::BuiltinOpResolver resolver;
-    if (InterpreterBuilder(*model, resolver)(&interpreter) != kTfLiteOk) {
-        LOGE("Cannot build interpreter");
-        return "ERROR: Cannot build interpreter";
-    }
-
-    if (interpreter->AllocateTensors() != kTfLiteOk) {
-        LOGE("Cannot allocate tensors");
-        return "ERROR: Cannot allocate tensors";
-    }
-
-    if (interpreter->Invoke() != kTfLiteOk) {
-        LOGE("Cannot invoke");
-        return "ERROR: Cannot invoke";
-    }
-
-
-
-    std::string output_message = "SIN([-8, 0.5, 2, 2.2, 201]) = [";
-
-    int num_outputs = interpreter->outputs().size();
-    if (num_outputs != 1) {
-        return "ERROR: unexpected number of outputs: " + std::to_string(num_outputs);
-    }
-    const TfLiteTensor* output_tensor = interpreter->output_tensor(0);
-    if (output_tensor->dims->size != 1) {
-        return "ERROR: unexpected output dimension " + std::to_string(output_tensor->dims->size);
-    }
-
-    int output_dim = (output_tensor->dims->data[0]);
-    for (int i=0; i<output_dim; i++) {
-        float output = interpreter->typed_output_tensor<float>(0)[i];
-        output_message += " " + std::to_string(output);
-    }
-
-    return output_message + "].";
-}
-
 }  // namespace tflite
+
+namespace {
 
 std::string jstring2string(JNIEnv *env, jstring jStr) {
     if (!jStr)
@@ -156,19 +207,16 @@ std::string jstring2string(JNIEnv *env, jstring jStr) {
     return ret;
 }
 
-/* This is a trivial JNI example where we use a native method
- * to return a new VM String. See the corresponding Java source
- * file located at:
- *
- *   app/src/main/java/com/example/hellolibs/MainActivity.java
- */
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_hellolibs_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz, jstring filePath) {
-    static std::once_flag onceFlag;
-    {
-        std::call_once ( onceFlag, [ ]{ tflite::ops::custom::Init(); } );
-    }
+}  // namespace
 
-    std::string message = tflite::RunInterpreter(jstring2string(env, filePath));
-    return env->NewStringUTF(message.c_str());
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_stupid_customops_MainActivity_initSvd(JNIEnv *env, jobject thiz, jstring modelPath) {
+    std::string output = tflite::ops::custom::PrepareInterpreter(jstring2string(env, modelPath));
+    return env->NewStringUTF(output.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_stupid_customops_MainActivity_runSvd(JNIEnv *env, jobject thiz) {
+    std::string output = tflite::ops::custom::runSvD();
+    return env->NewStringUTF(output.c_str());
 }
